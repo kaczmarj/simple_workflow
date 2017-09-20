b0ee988a75087522fc65f8b496d2b57eaedd26e6
jakubk <jakubk@mit.edu>
2017-09-20 10:25:57 -0400

    add matrix of dockerfiles

---
+++ build_all_images.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Build all Docker images using the Dockerfiles in this Directory.
+# Do this in parallel across 4 nodes.
+# https://github.com/nipy/nipype/blob/8d530d33e379daca1b3df8a3e1b78ffe2305c00c/.circle/tests.sh
+
+set -e
+set -u
+set -x
+
+if [ "${CIRCLE_NODE_TOTAL:-}" != "3" ]; then
+  echo "Builds are designed to run at 4x parallelism."
+  exit 1
+fi
+
+DOCKERHUB_USER_REPO="kaczmarj/regtests"
+
+DIR="$(dirname "$0")"
+FILE_PATTERN="${DIR}/Dockerfile.*"
+
+ALL_FILES="$(ls $FILE_PATTERN)"
+N_BUILDS_PER_NODE="$(( $(echo $ALL_FILES | xargs -n1 | wc -l) / $CIRCLE_NODE_TOTAL ))"
+
+
+case ${CIRCLE_NODE_INDEX} in
+  0)
+    for file in $(echo $ALL_FILES | xargs -n${N_BUILDS_PER_NODE} | head -1 | tail -1); do
+      tag="$(echo $(basename "$file") | cut -d. -f2-)"
+      # Retry building.
+      # https://github.com/nipy/nipype/blob/ad085ae86c9dbb97aedb316562ea31f79c5923fe/circle.yml
+      e=1 && for i in {1..5}; do
+        docker build -t "${DOCKERHUB_USER_REPO}:${tag}" - < $file && e=0 && break || sleep 15;
+      done && [ "$e" -eq "0" ]
+
+      docker push "${DOCKERHUB_USER_REPO}:${tag}"
+    done
+    ;;
+  1)
+    for file in $(echo $ALL_FILES | xargs -n${N_BUILDS_PER_NODE} | head -2 | tail -1); do
+      tag="$(echo $(basename "$file") | cut -d. -f2-)"
+      # Retry building.
+      # https://github.com/nipy/nipype/blob/ad085ae86c9dbb97aedb316562ea31f79c5923fe/circle.yml
+      e=1 && for i in {1..5}; do
+        docker build -t "${DOCKERHUB_USER_REPO}:${tag}" - < $file && e=0 && break || sleep 15;
+      done && [ "$e" -eq "0" ]
+
+      docker push "${DOCKERHUB_USER_REPO}:${tag}"
+    done
+    ;;
+  2)
+    for file in $(echo $ALL_FILES | xargs -n${N_BUILDS_PER_NODE} | head -3 | tail -1); do
+      tag="$(echo $(basename "$file") | cut -d. -f2-)"
+      # Retry building.
+      # https://github.com/nipy/nipype/blob/ad085ae86c9dbb97aedb316562ea31f79c5923fe/circle.yml
+      e=1 && for i in {1..5}; do
+        docker build -t "${DOCKERHUB_USER_REPO}:${tag}" - < $file && e=0 && break || sleep 15;
+      done && [ "$e" -eq "0" ]
+
+      docker push "${DOCKERHUB_USER_REPO}:${tag}"
+    done
+    ;;
+esac
---
+++ generate_dockerfiles.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Save Dockerfiles relative to this path so that this script can be run from
+# any directory. https://stackoverflow.com/a/246128/5666087
+DIR="$(dirname "$0")"
+
+BASE_IMAGES="centos:7.4.1708  debian:stretch-20170907 ubuntu:xenial-20170802"
+FLAGS=("--ants version=2.2.0" "--ants version=2.1.0" "--fsl version=5.0.10" "--fsl version=5.0.9")
+
+for base in $BASE_IMAGES; do
+  for flag in "${FLAGS[@]}"; do
+
+    if [[ "$base" == "centos"* ]]; then
+      pkg_manager="yum"
+    else
+      pkg_manager="apt"
+    fi
+
+    file_suffix1="$(echo "$base" | sed -e 's/\./-/g' -e 's/:/-/g' )"
+    file_suffix2="$(echo "$flag" | sed 's/--\(.*\)\ /\1/' | sed 's/version=/-/g')"
+    filename="${DIR}/Dockerfile.${file_suffix1}_${file_suffix2}"
+
+    docker run --rm kaczmarj/neurodocker:v0.3.0 generate \
+    --base "$base" --pkg-manager "$pkg_manager" \
+    --copy *.py environment.yml /opt/repronim/ \
+    --copy expected_output expected_output \
+    --miniconda env_name=bh_demo \
+    --run "conda env create --force --file /opt/repronim/environment.yml" \
+    $flag \
+    --workdir /opt/repronim/simple_workflow/scripts > $filename
+
+  done
+done
